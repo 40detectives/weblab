b7c16a66bd1000f2a3f5984bb213fd6865241c93
40detectives <9024463+40detectives@users.noreply.github.com>
2025-12-05 09:33:20 +0100

    fix(crl): fix the config to publish as multipage

---
+++ projects/conquering-responsive-layouts/vite.config.js
@@ -0,0 +1,43 @@
+import { defineConfig } from "vite";
+import path from "node:path";
+import fs from "node:fs";
+
+export default defineConfig({
+  build: {
+    outDir: "dist",
+    rolldownOptions: {
+      /* input: {
+        main: path.resolve(import.meta.dirname, "index.html"),
+        "01": path.resolve(import.meta.dirname, "01/*"),
+      }, */
+      input: getHtmlEntryFiles("challenges"),
+    },
+  },
+});
+
+function getHtmlEntryFiles(srcDir) {
+  const entry = {};
+
+  function traverseDir(currentDir) {
+    const files = fs.readdirSync(currentDir);
+
+    files.forEach((file) => {
+      const filePath = path.join(currentDir, file);
+      const isDirectory = fs.statSync(filePath).isDirectory();
+
+      if (isDirectory) {
+        // If it's a directory, recursively traverse it
+        traverseDir(filePath);
+      } else if (path.extname(file) === ".html") {
+        // If it's an HTML file, add it to the entry object
+        const name = path.relative(srcDir, filePath).replace(/\..*$/, "");
+        entry[name] = filePath;
+      }
+    });
+  }
+
+  traverseDir(srcDir);
+
+  console.log(entry);
+  return entry;
+}
