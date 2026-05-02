40421ca6c0e9f423320c6ba73a37d697bbd45933
40detectives <9024463+40detectives@users.noreply.github.com>
2026-05-02 13:42:35 +0200

    refactor(hub): update dir for Hub web app

---
+++ labs/hub/eslint.config.js
@@ -0,0 +1,90 @@
+/* css fundamentals v1 */
+import js from "@eslint/js";
+import json from "@eslint/json";
+import eslintConfigPrettier from "eslint-config-prettier";
+import { createTypeScriptImportResolver } from "eslint-import-resolver-typescript";
+import { importX } from "eslint-plugin-import-x";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import reactPlugin from "eslint-plugin-react";
+import reactHooks from "eslint-plugin-react-hooks";
+import refreshReact from "eslint-plugin-react-refresh";
+import { defineConfig, globalIgnores } from "eslint/config";
+import globals from "globals";
+/*
+import { createRequire } from "node:module";
+import viteConfig from "./vite.config.js";
+const require = createRequire(import.meta.url);
+const { createViteImportResolver } = require("eslint-import-resolver-vite");
+*/
+
+/** @type {import('eslint').Linter.Config[]} */
+export default defineConfig([
+  globalIgnores(["dist"]),
+  {
+    name: "eslint json recommended",
+    files: ["**/*.json"],
+    plugins: { json },
+    language: "json/json",
+    extends: ["json/recommended"],
+    ignores: ["./package-lock.json"],
+  },
+  {
+    name: "eslint json with comments recommended",
+    files: ["**/*.jsonc"],
+    plugins: { json },
+    language: "json/jsonc",
+    extends: ["json/recommended"],
+  },
+  {
+    name: "eslint js recommended",
+    files: ["**/*.{js,jsx,mjs,cjs}"],
+    plugins: { js },
+    extends: [js.configs.recommended, importX.flatConfigs.recommended],
+    languageOptions: {
+      globals: globals.browser,
+    },
+    rules: {
+      "no-unused-vars": ["error", { varsIgnorePattern: "^[_]" }],
+    },
+  },
+  {
+    name: "react related",
+    files: ["**/*.{js,jsx}"],
+    extends: [
+      refreshReact.configs.vite,
+      reactPlugin.configs.flat.recommended,
+      reactPlugin.configs.flat["jsx-runtime"],
+      reactHooks.configs.flat["recommended-latest"],
+      jsxA11y.flatConfigs.recommended,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        ecmaFeatures: { jsx: true },
+        sourceType: "module",
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+      "import-x/resolver-next": [
+        /* createNodeResolver({ extensions: [".js", ".jsx", ".mjs", ".cjs"] }), */
+        /* createViteImportResolver({ viteConfig }), */
+        createTypeScriptImportResolver({
+          project: ["jsconfig.json"],
+        }),
+      ],
+    },
+    rules: {
+      "react/prop-types": ["off"],
+    },
+  },
+  {
+    name: "eslint disable rules to match prettier",
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    extends: [eslintConfigPrettier],
+  },
+]);
