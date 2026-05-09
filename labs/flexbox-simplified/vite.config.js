29a7d4d538c99b44274dacc82e3f80524e6fe9ee
40detectives <9024463+40detectives@users.noreply.github.com>
2026-05-09 09:14:54 +0200

    feat(flex-simple): setup vite + vanilla JS sub-project

---
+++ labs/flexbox-simplified/vite.config.js
@@ -0,0 +1,16 @@
+import { defineConfig } from "vite";
+import path from "node:path";
+import { getHtmlEntryFiles } from "@weblab/shared/utils";
+
+export default defineConfig({
+  base: "/weblab/labs/flexbox-simplified/dist/",
+  build: {
+    outDir: "dist",
+    rolldownOptions: {
+      input: {
+        ...getHtmlEntryFiles("lessons"),
+        main: path.resolve(import.meta.dirname, "index.html"),
+      },
+    },
+  },
+});
